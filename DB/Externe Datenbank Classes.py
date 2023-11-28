ec0397a3303727d1e8291394f40f31df4f80326d
to <to@4.lol>
2023-11-28 12:06:27 +0100

    class.pyy?

---
+++ DB/Externe Datenbank Classes.py
@@ -0,0 +1,30 @@
+class Fahrzeugtyp(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    Fahrzeugart = db.Column(db.String(50))
+    Model = db.Column(db.String(50))
+    Wert = db.Column(db.Float)
+    Farbe = db.Column(db.String(30))
+
+class Eigentuemer(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    Vorname = db.Column(db.String(50), nullable=False)
+    Nachname = db.Column(db.String(50), nullable=False)
+
+class Besitzer(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    Vorname = db.Column(db.String(50), nullable=False)
+    Nachname = db.Column(db.String(50), nullable=False)
+    Besitzbeginn = db.Column(db.Date, nullable=False)
+    Besitzende = db.Column(db.Date)
+
+class Besitzer_Eigentuemer_Verwandschaft(db.Model):
+    Besitzer = db.Column(db.Integer, db.ForeignKey("Eigentuemer.ID"), nullable=False)
+    Eigentuemer = db.Column(db.Integer, db.ForeignKey("Besitzer.ID"), nullable=False)
+
+class Kennzeichen(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    Besitzer = db.Column(db.Integer, db.ForeignKey("Besitzer.ID"), nullable=False)
+    Fahrzeugtyp = db.Column(db.Integer, db.ForeignKey("Fahrzeugtyp.ID"))
+    KennzeichenText = db.Column(db.String(50), nullable=False)
+    Land = db.Column(db.String(3))
+    Eigentuemer = db.Column(db.Integer, db.ForeignKey("Eigentuemer.ID"), nullable=False)
---
+++ DB/Interne Datenbank Create.py
@@ -0,0 +1,45 @@
+class Gehalt(db.Model):
+    ID = db.Column(db.Integer, primary_key=True)
+    Durchschn_Gehalt = db.Column(db.Float, nullable=False)
+    Durchschn_Boni = db.Column(db.Float, nullable=False)
+
+class KontoInhaber(db.Model):
+	ID = db.Column(db.Integer, primary_key=True)
+	Vorname = db.Column(db.String(50), nullable=False)
+	Nachname = db.Column(db.String(50), nullable=False)
+
+class Konto(db.Model):
+	ID = db.Column(db.Integer, primary_key=True)
+	Inhaber = db.Column(db.Integer, db.ForeignKey("KontoInhaber.ID"), nullable=False)
+	Kontotyp = db.Column(db.String(20))
+	Kontonummer = db.Column(db.String(50), nullable=False)
+	Land = db.Column(db.String(3), nullable=False)
+	IBAN = db.Column(db.String(34), nullable=False)
+	Betrag = db.Column(db.DECIMAL, nullable=False)
+
+class Geldfluss(db.Model):
+	ID = db.Column(db.Integer, primary_key=True)
+	Verwendungszweck = db.Column(db.String(255))
+	Waehrung = db.Column(db.String(3), nullable=False)
+	EmpfaengerKonto = db.Column(db.Integer, ForeignKey("Konto.ID"), nullable=False)
+	AufraggeberKonto = db.Column(db.Integer, ForeignKey("Konto.ID"), nullable=False)
+	Betrag = db.Column(db.Float, nullable=False)
+
+class Projekt(db.Model):
+	ID = db.Column(db.Integer, primary_key=True)
+	Konto = db.Column(db.Integer, ForeignKey("Konto.ID"), nullable=False)
+	Name = db.Column(db.String(50), nullable=False)
+	Eingeplantes_Budget = db.Column(db.DECIMAL, nullable=False)
+	Eingetroffenes_Budget = db.Column(db.DECIMAL, nullable=False)
+
+class Beruf(db.Model):
+	ID = db.Column(db.Integer, primary_key=True)
+	Gehalt = db.Column(db.Integer, ForeignKey("Gehalt.ID"), nullable=False)
+	Berufname = db.Column(db.String(50), nullable=False)
+
+class Mitarbeiter(db.Model):
+	ID = db.Column(db.Integer, primary_key=True)
+	Beruf = db.Column(db.Integer, ForeignKey("Beruf.ID"), nullable=False)
+	Projekzugehoerigkeit = db.Column(db.Integer, ForeignKey("Projekt.ID"), nullable=False)
+	KontoInhaber = db.Column(db.Integer, ForeignKey("KontoInhaber.ID"), nullable=False)
+	Gehalt = db.Column(db.Integer, ForeignKey("Gehalt.ID"), nullable=False)
