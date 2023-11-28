9835c99aadfef57782a4505c81c45d2ce410ce7e
to <to@4.lol>
2023-11-28 09:45:26 +0100

    cut down main

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 Bootstrap5(app)
@@ -15,26 +14,6 @@ Bootstrap5(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy()
 db.init_app(app)
-
-class Test(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    year = db.Column(db.Integer, nullable=False)
-    description = db.Column(db.String(500), nullable=False)
-
-with app.app_context():
-    db.drop_all()
-    db.create_all()
-
-new_test = Test(
-    title="Test",
-    year=2002,
-    description="Test.",
-)
-
-with app.app_context():
-    db.session.add(new_test)
-    db.session.commit()
 
 @app.route("/")
 def home():
