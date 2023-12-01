397dc309319fcaad448ebdf245cfccc1c94478d6
jobote <120465415+jobote@users.noreply.github.com>
2023-12-01 11:34:57 +0100

    abc

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from flask import request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
@@ -5,28 +6,52 @@ import requests
 import header
 from header import app, db, DB, render_template
 
+
+class CreateForm(FlaskForm):
+    Nachname = StringField("Nachname", validators=[DataRequired()])
+    Vorname = StringField("Vorname", validators=[DataRequired()])
+    Projektname = StringField("Projektname", validators=[DataRequired()])
+    Versicherungsnummer = StringField("Versicherungsnummer", validators=[DataRequired()])
+    Mitarbeiter_ID = StringField("Mitarbeiter_ID", validators=[DataRequired()])
+    IBAN = StringField("IBAN", validators=[DataRequired()])
+    submit = SubmitField("Start person search")
+    submit2 = SubmitField("Start project analysis")
+
+
+
 @app.route("/")
 def home():
-    print(db.session.execute(db.select(DB.cis_classes.Beruf).order_by(DB.cis_classes.Beruf.ID)).scalar())
-    print(db.session.execute(db.select(DB.ext_classes.Eigentuemer).order_by(DB.ext_classes.Eigentuemer.ID)).scalar())
     Beruf = db.session.execute(db.select(DB.cis_classes.Beruf).order_by(DB.cis_classes.Beruf.ID)).scalars()
     return render_template("resultatseite.html", Beruf=Beruf)
+
 
 @app.route("/startseite")
 def startseite():
     return render_template("startseite.html", active_page='startseite')
 
+
 @app.route("/optionen")
 def optionen():
     return render_template("options.html", active_page='optionen')
+
 
 @app.route("/resultatseite")
 def resultatseite():
     return render_template("resultatseite.html", active_page='resultatseite')
 
-@app.route("/suche")
+
+@app.route("/suche", methods=["GET", "POST"])
 def suche():
-    return render_template("searchMenu.html", active_page='suche')
+    form = CreateForm()
+    if form.validate_on_submit():
+        Nachname = form.Nachname.data
+        Vorname = form.Vorname.data
+
+        print(Nachname)
+        print(Vorname)
+
+    return render_template("searchMenu.html", active_page='suche', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
